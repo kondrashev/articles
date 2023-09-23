81e9b4fce4f5d4830855314660d37e51e352f2aa
“kondrashev” <p.kondrashev@alterego.digital>
2023-09-23 21:57:23 +0300

    add argument "rejectValue" in "userActions"

--- client/store/actions/userActions.ts
+++ client/store/actions/userActions.ts
@@ -8,13 +8,19 @@ export interface IData {
   password: string;
 }
 
-export const checkAuthorization = createAsyncThunk<IUser, IData>('user/checkAuthorization', async ({ login, password }) => {
-  const response = await fetch(endpoints.signIn, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ login, password }),
-  });
-  const data: Promise<IUser> = response.json();
-  localStorage.setItem('token', `Bearer ${(await data).token}`);
-  return data;
-});
+export const checkAuthorization = createAsyncThunk<IUser, IData, { rejectValue: string }>(
+  'user/checkAuthorization',
+  async ({ login, password }, { rejectWithValue }) => {
+    const response = await fetch(endpoints.signIn, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ login, password }),
+    });
+    if (!response.ok) {
+      return rejectWithValue(`Error from server №${response.status} ${response.statusText}!!!`);
+    }
+    const data: Promise<IUser> = response.json();
+    localStorage.setItem('token', `Bearer ${(await data).token}`);
+    return data;
+  },
+);
