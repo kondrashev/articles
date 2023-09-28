9e3b5449f72a545e9449b3a88c8ca53e96ea7e6a
“kondrashev” <p.kondrashev@alterego.digital>
2023-09-28 18:28:12 +0300

    add mockup "avatar" in component "author.tsx"

--- client/components/author/author.tsx
+++ client/components/author/author.tsx
@@ -1,12 +1,20 @@
+import '@styles/Author';
+
+import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import React from 'react';
 
-import { IUser } from '../../../constants/constants';
 import { useAppSelector } from '../../hooks/hooks';
 
 const Author: React.FC = () => {
-  const getUser: IUser = useAppSelector((state) => state.usersReducer.user);
-  return <Box>{`Welcome ${getUser.login}!`}</Box>;
+  const login: string = useAppSelector((state) => state.usersReducer.user.login);
+
+  return (
+    <Box className="containerAuthor">
+      <Avatar className="avatar" src="images/avatar.jpg" />
+      <h5 className="login">{`Welcome ${login} !`}</h5>
+    </Box>
+  );
 };
 
 export default Author;
