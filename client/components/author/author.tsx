ca8a53ac7e668941e3a897138a127dc0fd88fa72
“kondrashev” <p.kondrashev@alterego.digital>
2023-09-29 16:36:32 +0300

    add component "AppMenu" in module "author.tsx"

--- client/components/author/author.tsx
+++ client/components/author/author.tsx
@@ -1,18 +1,14 @@
 import '@styles/Author';
 
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import React from 'react';
 
-import { useAppSelector } from '../../hooks/hooks';
+import AppMenu from './menu';
 
 const Author: React.FC = () => {
-  const login: string = useAppSelector((state) => state.usersReducer.user.login);
-
   return (
     <Box className="containerAuthor">
-      <Avatar className="avatar" src="images/avatar.jpg" />
-      <h5 className="login">{`Welcome ${login} !`}</h5>
+      <AppMenu />
     </Box>
   );
 };
---
+++ client/components/author/menu.tsx
@@ -0,0 +1,104 @@
+import AdbIcon from '@mui/icons-material/Adb';
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+import { useAppSelector } from '../../hooks/hooks';
+
+const settings = ['Add avatar', 'Add article', 'Logout'];
+
+const AppMenu: React.FC = () => {
+  const login: string = useAppSelector((state) => state.usersReducer.user.login);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (setting: string) => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            noWrap
+            variant="h6"
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+            }}
+          >
+            {login}
+          </Typography>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            noWrap
+            variant="h5"
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open menu">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar src="images/avatar.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              keepMounted
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default AppMenu;
