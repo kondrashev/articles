832b700461127d23d33e1db7b182f709bb0fdebf
“kondrashev” <p.kondrashev@alterego.digital>
2023-10-03 14:01:41 +0300

    add validator "checkAddArticle"

--- server/routes/authorRouter.ts
+++ server/routes/authorRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import endpoints from '../../constants/endpoints';
+import UserValidator from '../validators/index';
 // eslint-disable-next-line new-cap
 const router = Router();
 import AuthorController from '../controllers/authorController';
 import checkRole from '../middleware/checkRoleMiddleware';
-router.post(endpoints.addArticle, checkRole('AUTHOR'), AuthorController.addArticle);
+import errorHandler from '../middleware/ErrorHandlingMiddleware';
+router.post(endpoints.addArticle, checkRole('AUTHOR'), UserValidator.checkAddArticle(), errorHandler, AuthorController.addArticle);
 router.get(endpoints.getArticles, checkRole('AUTHOR'), AuthorController.getArticles);
 router.post(endpoints.deleteArticles, checkRole('AUTHOR'), AuthorController.deleteArticles);
 router.post(endpoints.updateArticle, checkRole('AUTHOR'), AuthorController.updateArticle);
