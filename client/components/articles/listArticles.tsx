a86dd64952777fd2914ce36c4c79ad08e85e7856
“kondrashev” <p.kondrashev@alterego.digital>
2023-11-01 12:57:18 +0200

    add component "AppPanel" in module "ListArticles"

--- client/components/articles/listArticles.tsx
+++ client/components/articles/listArticles.tsx
@@ -1,8 +1,16 @@
+import '@styles/listArticles';
+
 import Box from '@mui/material/Box';
 import React from 'react';
 
+import AppPanel from './appBar';
+
 const ListArticles: React.FC = () => {
-  return <Box>List articles!</Box>;
+  return (
+    <Box className="containerListArticles">
+      <AppPanel />
+    </Box>
+  );
 };
 
 export default ListArticles;
